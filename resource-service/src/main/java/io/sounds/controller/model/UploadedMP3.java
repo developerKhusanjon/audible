89f601951246cbf08d77b529b804e8742485143a
khusanjon_tuychiboev <developerkhusanjon@gmail.com>
2024-11-18 11:26:53 +0500

    Initial structure

---
+++ resource-service/src/main/java/io/sounds/controller/model/UploadedMP3.java
@@ -0,0 +1,8 @@
+package io.sounds.controller.model;
+
+import lombok.Data;
+
+@Data
+public class UploadedMP3 {
+    private Long id;
+}
---
+++ resource-service/src/main/java/io/sounds/service/ResourceService.java
@@ -0,0 +1,7 @@
+package io.sounds.service;
+
+import io.sounds.controller.model.UploadedMP3;
+
+public interface ResourceService {
+    UploadedMP3 uploadMP3();
+}
